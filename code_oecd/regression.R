2c6dfd84aedd7b2b770ab54718fe3c9e8a3c7a84
Bluebery Planterose <bluebery.planterose@sciencespo.fr>
2021-05-20 17:50:08 +0200

    CO2 index

--- code_oecd/regression.R
+++ code_oecd/regression.R
@@ -291,7 +291,7 @@ desc_table(dep_vars = c("survey_biased == 'No'", "survey_biased == 'Yes, right'"
 
 ## Indexes
 desc_table(dep_vars = c("index_affected", "index_knowledge", "index_knowledge_efa", "CO2_emission"), filename = "indexes",
-           dep.var.labels = c("Affected Index", "Knowledge Index", "Knowledge Index (EFA)", "CO2 emissions"),
+           dep.var.labels = c("Affected Index", "Knowledge Index", "Knowledge Index (EFA)", "CO_2 emissions"),
            dep.var.caption = c(""), data = us, indep_vars = c(control_variables, "core_metropolitan == 1"), indep_labels = c(cov_lab, "Core metropolitan"), mean_control = T
 )
 
@@ -320,26 +320,26 @@ dep.var.caption = c("Support"), data = us, indep_vars = c(control_variables, "in
 desc_table(dep_vars = c("standard_support > 0", "standard_public_transport_support > 0", "investments_support > 0", "tax_transfers_support > 0"
 ), filename = "support_w_indexes_CO2",
 dep.var.labels = c("Ban on combustion engine", "Ban on combustion engine with alternatives", "Green infrastructure program", "Carbon tax with cash transfers"),
-dep.var.caption = c("Support"), data = us, indep_vars = c(control_variables, "index_affected", "CO2_emission"), indep_labels = c(cov_lab, "Index affected", "CO2 emissions (t/year)"), mean_control = T
+dep.var.caption = c("Support"), data = us, indep_vars = c(control_variables, "index_affected", "CO2_emission"), indep_labels = c(cov_lab, "Index affected", "CO_2 emissions (t/year)"), mean_control = T
 )
 
-# C02 + index
+# CO2 + index
 desc_table(dep_vars = c("standard_support > 0", "standard_public_transport_support > 0", "investments_support > 0", "tax_transfers_support > 0"
 ), filename = "support_w_indexes_bothCO2",
 dep.var.labels = c("Ban on combustion engine", "Ban on combustion engine with alternatives", "Green infrastructure program", "Carbon tax with cash transfers"),
-dep.var.caption = c("Support"), data = us, indep_vars = c(control_variables, "index_affected", "index_knowledge", "CO2_emission"), indep_labels = c(cov_lab, "Index affected","Index knowledge", "CO2 emissions (t/year)"), mean_control = T
+dep.var.caption = c("Support"), data = us, indep_vars = c(control_variables, "index_affected", "index_knowledge", "CO2_emission"), indep_labels = c(cov_lab, "Index affected","Index knowledge", "CO_2 emissions (t/year)"), mean_control = T
 )
 
 # CO2 + EFA
 desc_table(dep_vars = c("standard_support > 0", "standard_public_transport_support > 0", "investments_support > 0", "tax_transfers_support > 0"
 ), filename = "support_w_indexes_CO2_efa",
 dep.var.labels = c("Ban on combustion engine", "Ban on combustion engine with alternatives", "Green infrastructure program", "Carbon tax with cash transfers"),
-dep.var.caption = c("Support"), data = us, indep_vars = c(control_variables, "index_affected", "index_knowledge_efa", "CO2_emission"), indep_labels = c(cov_lab, "Index affected","Index knowledge EFA", "CO2 emissions (t/year)"), mean_control = T
+dep.var.caption = c("Support"), data = us, indep_vars = c(control_variables, "index_affected", "index_knowledge_efa", "CO2_emission"), indep_labels = c(cov_lab, "Index affected","Index knowledge EFA", "CO_2 emissions (t/year)"), mean_control = T
 )
 
 # All 3
 desc_table(dep_vars = c("standard_support > 0", "standard_public_transport_support > 0", "investments_support > 0", "tax_transfers_support > 0"
 ), filename = "support_w_indexes_all_three",
 dep.var.labels = c("Ban on combustion engine", "Ban on combustion engine with alternatives", "Green infrastructure program", "Carbon tax with cash transfers"),
-dep.var.caption = c("Support"), data = us, indep_vars = c(control_variables, "index_affected", "index_knowledge", "index_knowledge_efa", "CO2_emission"), indep_labels = c(cov_lab, "Index affected","Index knowledge", "Index knowledge EFA", "CO2 emissions (t/year)"), mean_control = T
+dep.var.caption = c("Support"), data = us, indep_vars = c(control_variables, "index_affected", "index_knowledge", "index_knowledge_efa", "CO2_emission"), indep_labels = c(cov_lab, "Index affected","Index knowledge", "Index knowledge EFA", "CO_2 emissions (t/year)"), mean_control = T
 )
