ea3edde590cfc2a44e760a503957f93584245201
Bluebery Planterose <bluebery.planterose@sciencespo.fr>
2021-09-07 17:13:11 +0200

    zip region BR

--- code_oecd/zipcodes/create_zip_list.R
+++ code_oecd/zipcodes/create_zip_list.R
@@ -15,9 +15,12 @@ library(data.table)
 # data$zip_start[data$zip_start>10] <- data$zip_start[data$zip_start>10]/10
 # data <- data[c(2,1)]
 
+# 4. BR
+# data <- read.csv(file = "/Users/Bluebii/Library/Mobile Documents/com~apple~CloudDocs/TRAVAIL/Jobs/Stantcheva_2020:21/OECD/oecd_climate/data/zipcodes/BR_region_quota_zip.csv", header = T, sep = ";")
+
 # Change this var. in function of the format
-zip.size = 10000
-# 1,000 ESP; 10,000 CH/ID
+zip.size = 100
+# 1,000 ESP; 10,000 CH/ID; 100 BR
 
 #1.ESP
 
@@ -26,6 +29,10 @@ generate_zip <- function(vector){
   interval.start <- as.numeric(vector[[2]])*zip.size
 #  if(vector[[2]] < 9){
   interval.end <- (as.numeric(vector[[2]])+1)*zip.size-1
+
+  # BR
+  # interval.end <- (as.numeric(vector[[3]]))*zip.size-1
+  
 #  }
 #  else if(vector[[2]]>=9)
 #  {
@@ -43,8 +50,10 @@ result <- apply(data, 1, generate_zip)
 result <- rbindlist(result, fill=T)
 
 #1.ESP
-# write.csv(result,"ES_zipcode.csv", row.names=T)
+# write.csv(result,"ES_zipcode.csv", row.names=F)
 #2. CH
-# write.csv(result,"CH_zipcode.csv", row.names=T)
+# write.csv(result,"CH_zipcode.csv", row.names=F)
 # 3. ID
-# write.csv(result,"ID_zipcode.csv", row.names=T)
+# write.csv(result,"ID_zipcode.csv", row.names=F)
+# 4. BR
+# write.csv(result, "BR_zipcode.csv", row.names = F)
