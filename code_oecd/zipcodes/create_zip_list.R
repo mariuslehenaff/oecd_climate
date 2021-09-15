bea2c32ef1604f12599f7a0dccc9044bd29535f5
Bluebery Planterose <bluebery.planterose@sciencespo.fr>
2021-09-15 15:57:01 +0200

    Update zip CN

--- code_oecd/zipcodes/create_zip_list.R
+++ code_oecd/zipcodes/create_zip_list.R
@@ -6,9 +6,9 @@ library(data.table)
 # data <- read.csv(file = "/Users/Bluebii/Downloads/zip_spain.csv", header=T, sep=";")
 
 # 2. CH
-# data <- read.csv(file = "/Users/Bluebii/Library/Mobile Documents/com~apple~CloudDocs/TRAVAIL/Jobs/Stantcheva_2020:21/OECD/oecd_climate/data/zipcodes/china_region_quota_zip.csv", header=T, sep=";")
-# data<-data %>%
-#  select(Region_quota, zip_start)
+ data <- read.csv(file = "/Users/Bluebii/Library/Mobile Documents/com~apple~CloudDocs/TRAVAIL/Jobs/Stantcheva_2020:21/OECD/oecd_climate/data/zipcodes/CN_region_quota_zip.csv", header=T, sep=";")
+data<-data %>%
+ select(Region_quota, zip_start)
 
 # 3. ID
 # data <- read.csv(file="/Users/Bluebii/Library/Mobile Documents/com~apple~CloudDocs/TRAVAIL/Jobs/Stantcheva_2020:21/OECD/oecd_climate/data/zipcodes/Indonesia_zipcode.csv", header=T, sep=";")
@@ -19,7 +19,7 @@ library(data.table)
 # data <- read.csv(file = "/Users/Bluebii/Library/Mobile Documents/com~apple~CloudDocs/TRAVAIL/Jobs/Stantcheva_2020:21/OECD/oecd_climate/data/zipcodes/BR_region_quota_zip.csv", header = T, sep = ";")
 
 # Change this var. in function of the format
-zip.size = 100
+zip.size = 10000
 # 1,000 ESP; 10,000 CH/ID; 100 BR
 
 #1.ESP
@@ -38,6 +38,16 @@ generate_zip <- function(vector){
 #  {
 #    interval.end <- (as.numeric(vector[[2]]))*zip.size+999
 #  }
+  
+  # CH
+  # if(vector[[2]] < 100){
+  #   interval.end <- (as.numeric(vector[[2]])+1)*zip.size-1
+  # }
+  # else if(vector[[2]] > 100){
+  #   interval.start <- interval.start/zip.size
+  #   interval.end <- interval.start # Account for Macao, HK, Taiwan
+  # }
+  # 
   zip <- c(interval.start:interval.end)
 
   df <- data.frame(zip, vector[[1]]) %>%
@@ -52,7 +62,7 @@ result <- rbindlist(result, fill=T)
 #1.ESP
 # write.csv(result,"ES_zipcode.csv", row.names=F)
 #2. CH
-# write.csv(result,"CH_zipcode.csv", row.names=F)
+write.csv(result,"CH_zipcode.csv", row.names=F)
 # 3. ID
 # write.csv(result,"ID_zipcode.csv", row.names=F)
 # 4. BR
